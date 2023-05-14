b69bfb60134309477633fc94237e31bcfdba81b0
Sreeharie123 <sreehariengoli@gmail.com>
2023-05-14 11:39:38 +0530

    Database connected

---
+++ index.ts
@@ -0,0 +1,24 @@
+import mongoose, { Mongoose } from "mongoose"
+import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+//Variables
+const app=express()
+const port=5000
+dotenv.config()
+const mongoDbURL=process.env.MONGO_DB_URL as string
+
+//MiddleWares
+app.use(cors())
+app.use(express.json())
+
+
+//MongoDB Connection
+mongoose.connect(mongoDbURL).then(()=>{
+    console.log("database connected successfully")
+})
+
+app.listen(process.env.PORT||port,()=>{
+    console.log(`port listening ${port}`)
+})
