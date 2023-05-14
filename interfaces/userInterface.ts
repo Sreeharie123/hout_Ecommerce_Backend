07422e07d75a8d2a2d72f28d7bb5985d124c20cd
Sreeharie123 <sreehariengoli@gmail.com>
2023-05-14 16:18:50 +0530

    password bycrypt

--- interfaces/userInterface.ts
+++ interfaces/userInterface.ts
@@ -6,3 +6,64 @@ export interface user{
     password:string,
     role:string
 }
+
+ export interface userDetails {
+    '$__': _;
+    '$isNew': boolean;
+    _doc: Doc;
+  }
+  
+ export interface Doc {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  }
+  
+  interface _ {
+    activePaths: ActivePaths;
+    skipId: boolean;
+  }
+  
+  interface ActivePaths {
+    paths: Paths;
+    states: States;
+  }
+  
+  interface States {
+    require: Require;
+    default: Require;
+    init: Init;
+  }
+  
+  interface Init {
+    _id: boolean;
+    firstName: boolean;
+    lastName: boolean;
+    email: boolean;
+    password: boolean;
+    role: boolean;
+    createdAt: boolean;
+    updatedAt: boolean;
+    __v: boolean;
+  }
+  
+  interface Require {
+  }
+  
+  interface Paths {
+    password: string;
+    email: string;
+    lastName: string;
+    firstName: string;
+    role: string;
+    _id: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: string;
+  }
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,32 +1,46 @@
 import express from "express";
 import { UserModel } from "../models/userSchema";
-import { user } from "../interfaces/userInterface";
+import { user, userDetails } from "../interfaces/userInterface";
+import bcrypt from "bcrypt"
 const router = express.Router();
 
 //Create User
 router.post("/register", async (req, res) => {
   const { error } = req.body;
-  if (error) return res.status(500).send(error[0].message);
+  if (error) return res.status(500).json(error[0].message);
+
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(req.body.password, salt)
+
   const newUser = new UserModel<user>({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPassword,
     role: req.body.role,
   });
   try {
     const saveUser = await newUser.save();
     res.status(200).json(saveUser);
   } catch (error) {
-    res.status(500).json("you are all ready registered login please");
+    res.status(500).json("Email is already exist");
   }
 });
 
 router.post("/login", async (req, res) => {
+    const {error}=req.body;
+    if(error) return res.status(500).json(error[0].message)
   try {
-    const userEmail = await UserModel.findOne({ email: req.body.email });
-    if (!userEmail) return res.status(400).json("Sign up please");
-    res.send(userEmail);
+    const user  = await UserModel.findOne<userDetails>({ email: req.body.email })
+    if (!user) return res.status(400).json("There is no user with this email");
+
+    const realPassword=await bcrypt.compare(req.body.password,user._doc.password)
+    if(!realPassword) return res.status(400).json("password is wrong")
+
+    const{password,...others} =user._doc
+
+    res.status(200).json(others)
+
   } catch (error) {
     res.status(500).json(error);
   }
