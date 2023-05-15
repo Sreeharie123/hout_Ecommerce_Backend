3a9bd2eef341f250ab72a413c6cce20c7b34bac8
Sreeharie123 <sreehariengoli@gmail.com>
2023-05-15 22:09:31 +0530

    jwt function completed

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,16 +2,15 @@ import express from "express";
 import { UserModel } from "../models/userSchema";
 import { user, userDetails } from "../interfaces/userInterface";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const router = express.Router();
 
 //Create User
 router.post("/register", async (req, res) => {
-  const { error } = req.body;
-  if (error) return res.status(500).json(error[0].message);
 
+  try {
   const salt = await bcrypt.genSalt(10)
   const hashPassword = await bcrypt.hash(req.body.password, salt)
-
   const newUser = new UserModel<user>({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -19,7 +18,7 @@ router.post("/register", async (req, res) => {
     password: hashPassword,
     role: req.body.role,
   });
-  try {
+ 
     const saveUser = await newUser.save();
     res.status(200).json(saveUser);
   } catch (error) {
@@ -29,8 +28,6 @@ router.post("/register", async (req, res) => {
 
 //login User
 router.post("/login", async (req, res) => {
-    const {error}=req.body;
-    if(error) return res.status(500).json(error[0].message)
   try {
     const user  = await UserModel.findOne<userDetails>({ email: req.body.email })
     if (!user) return res.status(400).json("There is no user with this email");
@@ -38,9 +35,16 @@ router.post("/login", async (req, res) => {
     const realPassword=await bcrypt.compare(req.body.password,user._doc.password)
     if(!realPassword) return res.status(400).json("password is wrong")
 
+    const Secrete_jwt_Key=process.env.JWT_KEY as string
+    
+    const accessToken = jwt.sign({
+      id:user._doc._id,
+      role:user._doc.role,
+      firstName:user._doc.firstName
+    },Secrete_jwt_Key,{expiresIn:"3d"})
+    
     const{password,...others} =user._doc
-
-    res.status(200).json(others)
+    res.status(200).json({...others,accessToken})
 
   } catch (error) {
     res.status(500).json(error);
